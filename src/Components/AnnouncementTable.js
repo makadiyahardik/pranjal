1a525f2eee160b49fa4675d5760cc6204c941059
hardikmakadiya <hardikmakadiya8@gmail.com>
2024-10-10 16:59:28 +0530

    added Corporate Announcements tabel

---
+++ src/Components/AnnouncementTable.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import SortingArrow from './SortingArrow/SortingArrow';
+
+const AnnouncementTable = (props) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5);
+  const [sortConfig, setSortConfig] = useState(null);
+
+  const handleSort = (key) => {
+    let direction = 'ascending';
+    if (sortConfig?.key === key && sortConfig?.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
+
+  const sortedData = [...props.data].sort((a, b) => {
+    if (!sortConfig) return 0;
+    const { key, direction } = sortConfig;
+    if (a[key] < b[key]) return direction === 'ascending' ? -1 : 1;
+    if (a[key] > b[key]) return direction === 'ascending' ? 1 : -1;
+    return 0;
+  });
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = sortedData.slice(indexOfFirstItem, indexOfLastItem);
+
+  const nextPage = () => {
+    if (currentPage < Math.ceil(sortedData.length / itemsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  return (
+    <div className="container mx-auto p-4 ">
+      <table className="min-w-full table-auto border-collapse border border-gray-200 rounded-[40px]">
+        <thead>
+          <tr>
+            {props.TableHeads.map((item, index) => (
+              <th
+                key={index}
+                className="px-4 py-4 cursor-pointer font-semibold text-[12px] leading-[18px] text-[#475467] whitespace-nowrap text-center items-center justify-center"
+                onClick={() => handleSort(item.key)}
+              >
+                <div className="flex justify-center items-center">
+                  <p>{item.label}</p>
+                  <SortingArrow
+                    isSorted={sortConfig?.key === item.key}
+                    direction={
+                      sortConfig?.key === item.key
+                        ? sortConfig?.direction
+                        : 'descending'
+                    }
+                  />
+                </div>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((item, index) => (
+            <tr key={index} className="hover:bg-[#D4EAFF] border border-b-1 bg-gray-100">
+              <td className="text-center px-4 py-8 font-medium text-[14px] text-[#101828]">{item.label}</td>
+              <td className="whitespace-nowrap text-center px-4 py-8 font-medium text-[14px] text-[#101828]">{item.date}</td>
+              <td className= "whitespace-nowrap text-center px-4 py-8 font-medium text-[14px] text-[#101828]">{item.time}</td>
+              <td className="text-center px-4 py-8 font-medium text-[14px] text-[#101828]">{item.description}</td>
+              <td
+  className={`text-center px-4 py-8 font-medium text-[14px] leading-[20px] cursor-pointer`}
+>
+  <a href="../Assets/Files/your-pdf-file.pdf" target="_blank" rel="noopener noreferrer">
+    <img src="../Assets/Images/PDF.svg" alt="Open PDF" />
+  </a>
+</td>
+
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="flex justify-between mt-4">
+        <div className="flex gap-3 justify-center items-center">
+          <button
+            onClick={prevPage}
+            className="NewInputTpeV3Shodow border border-[#D0D5DD] rounded-[12px] bg-white flex gap-2 items-center justify-center h-[44px] w-auto font-semibold px-4 py-2 disabled:opacity-50"
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          <button
+            onClick={nextPage}
+            className="NewInputTpeV3Shodow border border-[#D0D5DD] rounded-[12px] bg-white flex gap-2 items-center justify-center h-[44px] w-auto px-4 font-semibold disabled:opacity-50"
+            disabled={currentPage === Math.ceil(sortedData.length / itemsPerPage)}
+          >
+            Next
+          </button>
+        </div>
+        <span>
+          Page {currentPage} of {Math.ceil(sortedData.length / itemsPerPage)}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default AnnouncementTable;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Table from '../Components/Table';
 import Summary from '@/molecules/Summary';
 import YearSelection from '@/Components/YearSelection/YearSelection';
 import ADDvTable from '@/Components/ADDvTable';
+import ADDVSockersTable from '@/Components/ADDVSockersTable';
+import AnnouncementTable from '@/Components/AnnouncementTable';
 
 export default function Home() {
   const customers = {
@@ -174,7 +176,7 @@ export default function Home() {
       movePercent: '33%',
     },
     {
-      rank:2,
+      rank: 2,
       monthyear: 'APR,24',
       tradedQty: 6825600,
       vwap: 120,
@@ -228,14 +230,6 @@ export default function Home() {
       label: '% Move',
       key: 'movePercent',
     },
-   
-   
- 
-  
-   
-    
-   
- 
   ];
   const [showYears, setShowYears] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
@@ -244,7 +238,416 @@ export default function Home() {
   const toggleYearSelector = () => {
     setShowYears(!showYears);
   };
+  const dummyData = [
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: -5,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 330,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 336,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 336,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 331,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 335,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 313,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 93,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 23,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 43,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 3,
+    },
+  ];
 
+  const dummyDataADDVSockers = [
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: -5,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Apr, 24',
+      high: 140,
+      low: 210,
+      hLPercentage: '8%',
+      movePercentage: 40,
+    },
+    {
+      monthYear: 'Apr, 24',
+      high: 150,
+      low: 220,
+      hLPercentage: '9%',
+      movePercentage: 50,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: -5,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Apr, 24',
+      high: 140,
+      low: 210,
+      hLPercentage: '8%',
+      movePercentage: 40,
+    },
+    {
+      monthYear: 'Apr, 24',
+      high: 150,
+      low: 220,
+      hLPercentage: '9%',
+      movePercentage: 50,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: -5,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Mar, 24',
+      high: 130,
+      low: 200,
+      hLPercentage: '7%',
+      movePercentage: 33,
+    },
+    {
+      monthYear: 'Apr, 24',
+      high: 140,
+      low: 210,
+      hLPercentage: '8%',
+      movePercentage: 40,
+    },
+    {
+      monthYear: 'Apr, 24',
+      high: 150,
+      low: 220,
+      hLPercentage: '9%',
+      movePercentage: 50,
+    },
+  ];
+
+  const ADVSockersTabelHeads = [
+    { label: 'Month, Year', key: 'monthYear' },
+    { label: 'High', key: 'high' },
+    { label: 'Low', key: 'low' },
+    { label: 'H-L %', key: 'hLPercentage' },
+    { label: '% Move', key: 'movePercentage' },
+  ];
+  const AnnouncementTabelHeads = [
+    { label: 'Arvind Fashion Ltd', key: 'label' },
+    { label: 'Date', key: 'date' },
+    { label: 'Time', key: 'time' },
+    { label: 'Description', key: 'description' },
+    { label: 'PDF Link', key: 'pdf' },
+  ];
+  const AnnouncementTabelData = [
+    {
+      id: 1,
+      label:
+        'Announcement under Regulation 30 (LODR) - Press Release / Media Release',
+      date: '26 Oct, 24',
+      time: '9:47 AM',
+      description:
+        'Bharat Forge Limited has informed the Exchange regarding a press release dated October 02, 2024, titled Kalyani Strategic Systems Ltd from India and US-Based A',
+      pdf: '',
+    },
+  ];
   return (
     <div className="flex flex-col w-full lg:max-w-[1280px] justify-start items-start mx-auto mt-10 px-5">
       <Summary customers={customers} members={members} active={active} />
@@ -298,29 +701,85 @@ export default function Home() {
         Heading="Top 20 Delivery Days"
         data={top20DaysData}
       />
-        <div className="flex w-full justify-start mt-10">
+      <div className="flex w-full justify-start mt-10">
         <h1 className="text-[24px] font-semibold text-[#101828]">
-        ADDV Shockers
+          ADDV Shockers
         </h1>
       </div>
-    <div className='flex flex-col lg:flex-row gap-4 w-full mt-8'>
-      <div className='flex flex-col gap-3 w-full'>
-      <h1 className="text-[24px] font-semibold text-[#101828]">
-      Top 5 ADDV Months
-        </h1>
-      <ADDvTable TableHeads={DDVTabelHeads}
-        Heading="Top 20 Delivery Days"
-        data={ADDvData}/>
+      <div className="flex flex-col lg:flex-row gap-4 w-full mt-8">
+        <div className="flex flex-col gap-3 w-full">
+          <h1 className="text-[24px] font-semibold text-[#101828]">
+            Top 5 ADDV Months
+          </h1>
+          <ADDvTable
+            TableHeads={DDVTabelHeads}
+            Heading="Top 20 Delivery Days"
+            data={ADDvData}
+          />
+        </div>
+        <div className="flex flex-col gap-3 w-full">
+          <h1 className="text-[24px] font-semibold text-[#101828]">
+            Lowest 5 Addv Months
+          </h1>
+          <ADDvTable
+            TableHeads={DDVTabelHeads}
+            Heading="Top 20 Delivery Days"
+            data={ADDvData}
+          />
+        </div>
       </div>
-      <div className='flex flex-col gap-3 w-full'>
-      <h1 className="text-[24px] font-semibold text-[#101828]">
-      Lowest 5 Addv Months
-        </h1>
-      <ADDvTable TableHeads={DDVTabelHeads}
-        Heading="Top 20 Delivery Days"
-        data={ADDvData}/>
+
+      <div className="flex flex-col lg:flex-row gap-4 w-full mt-8">
+        <div className="flex flex-col gap-3 w-full">
+          <h1 className="text-[24px] font-semibold text-[#101828]">
+            ADDV Shockers
+          </h1>
+          <ADDVSockersTable
+            TableHeads={ADVSockersTabelHeads}
+            Heading="Top 20 Delivery Days"
+            data={dummyDataADDVSockers}
+          />
+        </div>
+        <div className="flex flex-col gap-3 w-full">
+          <h1 className="text-[24px] font-semibold text-[#101828]">
+            ADDV Shockers
+          </h1>
+          <ADDVSockersTable
+            TableHeads={ADVSockersTabelHeads}
+            Heading="Top 20 Delivery Days"
+            data={dummyDataADDVSockers}
+          />
+        </div>
       </div>
-    </div>
+      <div className="flex w-full justify-start mt-10">
+        <h1 className="text-[24px] font-semibold text-[#101828]">
+          Corporate Announcements
+        </h1>
+      </div>
+      <div className="flex w-full flex-col gap-4 xs:flex-row xs:justify-between items-center mt-5">
+        <div className="relative">
+          <div className="NewInputTpeV3Shodow border border-[#EAECF0] rounded-[12px] bg-white flex gap-2 items-center justify-center h-[44px] w-auto p-2 cursor-pointer">
+            <img src="../Assets/Images/bar.svg" alt="logo" />
+            <p className="text-[#344054] text-[14px] font-semibold leading-[20px]">
+              {' '}
+              More filters
+            </p>
+          </div>
+        </div>
+        <div className="NewInputTpeV3Shodow border border-[#EAECF0] rounded-[12px] bg-white flex gap-2 items-center justify-center h-[44px] xs:w-[354px] w-[330px] p-2">
+          <img className="" src="../Assets/Images/Flower.svg" alt="logo" />
+          <p className="text-[#344054] text-[14px] font-semibold leading-[20px]">
+            Vwap of top 10 delivery days of {selectedYear}:{' '}
+          </p>
+          <p className="text-black text-[14px] font-bold leading-[20px]">
+            {top20DaysData?.length}{' '}
+          </p>
+        </div>
+      </div>
+      <AnnouncementTable
+        TableHeads={AnnouncementTabelHeads}
+        data={AnnouncementTabelData}
+      />
     </div>
   );
 }
