ffd6011f87939e71bd7b4ff2dec8741c2627323c
hardikmakadiya <hardikmakadiya8@gmail.com>
2025-01-03 12:59:38 +0530

    text changes

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -13,7 +13,7 @@ const Login = () => {
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight mx-auto flex items-center justify-center tracking-tight text-[#101828] md:text-2xl dark:text-white">
-          Log in to Atoms
+          Log 
           </h1>
           <p className="text-[#475467] text-[16px] leading-[24px] mx-auto flex items-center justify-center e">
           Connecting dots
